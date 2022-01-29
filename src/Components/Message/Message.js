4cfe6e42fa10280c527d1cff311e33c7694f7a35
Ewuranna Smith-Quayson <qusmew@gmail.com>
2022-01-29 00:11:30 +0000

    updated links

--- src/Components/Message/Message.js
+++ src/Components/Message/Message.js
@@ -8,7 +8,7 @@ class Message extends React.Component{
         You can do any of these three things to celebrate with me today:<br/> 
        <br/> <b> Read</b> any of <a href="https://vocal.media/authors/ewuranna-smith-quayson" >my articles</a> and share with 10 people.<br/>
        <br/> <b>Send cash</b> (because who doesn't like cash right?). 
-        Options: <a href=''> Mobile Money</a>, <a href=''> Debit Card</a> or  <a href=''> Paypal</a>
+        Options: <a href=''> Mobile Money</a>, <a href=''> Debit Card</a>  or  <a href="https://www.paypal.com/paypalme/buzzedison"> Paypal</a>
         <br/><br/>
         <b>Buy </b> from any of these businesses:  <a href='https://www.justfreshonline.com/juices'> Just Fresh</a> (juices) or <a href='https://www.pricelezz.co'>Pricelezz</a> (A website or ecommerce shop at a really good deal)
          <br/><br/> <br/>
